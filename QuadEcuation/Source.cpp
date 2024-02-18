6bbb9b6c5d5284ba37d5e5445dccf85021bea6a1
Melkoten <muryginperm@gmail.com>
2024-02-18 22:01:30 +0300

    Delete flag, removed PrintX from Solve

--- QuadEcuation/Source.cpp
+++ QuadEcuation/Source.cpp
@@ -3,44 +3,37 @@
 #include <stdlib.h>
 #include <math.h>
 
-void Scan—oef(float* a, float* b, float* c) {
+void Scan—oef(double* a, double* b, double* c) {
 	*a = 0;
-	while(*a==0) {
+	while(*a==0.0) {
 	printf("\nEnter coefficient 'a': ");
-	scanf_s("%f", a);
-	 if (*a == 0) printf("Coef 'a' shouldn't be zero");
+	scanf_s("%lf", a);
+	 if (*a == 0.0) printf("Coef 'a' shouldn't be zero");
 	}
 	printf("\nEnter coefficient 'b': ");
-	scanf_s("%f", b);
+	scanf_s("%lf", b);
 	printf("\nEnter coefficient 'c': ");
-	scanf_s("%f", c);
+	scanf_s("%lf", c);
 	printf("\nYour ecuation: %f*x^2+(%f*x)+(%f)", *a, *b, *c);
 }
-void PrintX(int flag, float X[2]) {
-	if (flag == 0) printf("\nThere are no Real roots of ecuation");
-	if (flag == 1) printf("\nRoot is: X=%f", X[0]);
-	if (flag == 2) printf("\nRoots are: X1=%f, X2=%f", X[0], X[1]);
+void PrintX(double X[2], double D) {
+	if (D==0.0) printf("\nRoot is: X=%lf", X[0]);
+	if (D>0.0) printf("\nRoots are: X1=%lf, X2=%lf", X[0], X[1]);
+	if (D < 0.0) printf("\n Discriminant is under 0, no real roots");
 }
-void SolveEcuation(float a, float b, float c) {
-	float D;
-	float X[2];
-	int flag = 0;
-	D = b * b - 4 * a * c;
-	if (D == 0) {
-		flag = 1;
-		X[0] = - b / 2 / a;
+void SolveEcuation(double a, double b, double c, double *D, double *X) {
+	*D = b * b - 4.0 * a * c;
+	if (*D >= 0.0) {
+		X[0] = (-b + sqrt(*D)) / 2.0 / a;
+		X[1] = (-b - sqrt(*D)) / 2.0 / a;
 	}
-	if (D > 0) {
-		flag = 2;
-		X[0] = (-b + sqrt(D)) / 2 / a;
-		X[1] = (-b - sqrt(D)) / 2 / a;
-	}
-	PrintX(flag, X);
 }
 
 int main(void) {
-	float a, b, c;
+	double a, b, c, D;
+	double X[2];
 	Scan—oef(&a, &b, &c);
-	SolveEcuation(a, b, c);
+	SolveEcuation(a, b, c, &D, X);
+	PrintX(X, D);
 	return 0;
 }
